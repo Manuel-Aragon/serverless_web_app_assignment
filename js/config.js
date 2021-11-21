4e4744096b72ee13ab2d67e668a75383aabaff71
Manuel Aragon <aragon.manuel@live.com>
2021-11-21 16:10:03 -0600

    added client

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_pHJCh0fpI
-        userPoolClientId: '', // e.g. 5tiq565u0nd9sv95n9v768p71h
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_pHJCh0fpI', // e.g. 
+        userPoolClientId: '5tiq565u0nd9sv95n9v768p71h', // e.g. 
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
